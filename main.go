b9192c4150ba272c5ad249711611df27dc0e9019
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-07-20 15:35:29 +0500

    Routing requests

--- main.go
+++ main.go
@@ -9,9 +9,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Gists"))
 }
 
+// Add a gistView hadler function
+func gistView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display specific gist..."))
+}
+
+// Add a gistCreate handler
+func gistCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new gist"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home) // Restrict this route to exact match / only
+	mux.HandleFunc("/gist/view", gistView)
+	mux.HandleFunc("/gist/create", gistCreate)
 
 	log.Print(("starting server on :4000"))
 
