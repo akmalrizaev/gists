c7fac30178f519b5d0d12ac6328fe0d112981feb
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-07-22 09:49:26 +0500

    Serving static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileserver := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileserver))
+
 	mux.HandleFunc("GET /{$}", home)                // Restrict this route to exact match / only
 	mux.HandleFunc("GET /gist/view/{id}", gistView) // Add the {id} wildcard segment
 	mux.HandleFunc("GET /gist/create", gistCreate)
