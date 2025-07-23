6fe74a7720bfe09fc9490c3c9582930f76916538
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-07-23 18:08:19 +0500

    Executing SQL statements

--- internal/gists.go
+++ internal/gists.go
@@ -19,7 +19,7 @@ type GistModel struct {
 
 func (m *GistModel) Insert(title string, content string, expires int) (int, error) {
 
-	stmt := `INSERT INTO snippets (title, content, created, expires)
+	stmt := `INSERT INTO gists (title, content, created, expires)
     VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
 	result, err := m.DB.Exec(stmt, title, content, expires)
